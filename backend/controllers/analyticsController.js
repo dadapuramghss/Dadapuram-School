217279fd81ec497e1a8307866a88c6c1784d419e
Aravindh V <dadapuramghss@gmail.com>
2026-09-15 20:46:59 +0530

    fix(backend): inject dynamic maximum marks calculation into Classwise First Marks pipeline

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -592,9 +592,9 @@ const getDashboardStats = async (req, res) => {
               initialValue: [],
               in: {
                 $cond: [
-                  { $in: ["$$this.subject", "$$value.subject"] },
-                  "$$value",
-                  { $concatArrays: ["$$value", ["$$this"]] }
+                  { $in: ["$this.subject", "$value.subject"] },
+                  "$value",
+                  { $concatArrays: ["$value", ["$this"]] }
                 ]
               }
             }
@@ -614,7 +614,76 @@ const getDashboardStats = async (req, res) => {
           totalScore: { $sum: "$terms.marks.score" }
         }
       },
-      { $sort: { totalScore: -1 } },
+      {
+        $lookup: {
+          from: "classconfigs",
+          let: { std: "$_id.standard", sec: "$_id.section" },
+          pipeline: [
+            { $match:
+               { $expr:
+                  { $and:
+                     [
+                       { $eq: [ "$standard",  "$std" ] },
+                       { $eq: [ "$section", "$sec" ] }
+                     ]
+                  }
+               }
+            }
+          ],
+          as: "config"
+        }
+      },
+      {
+        $addFields: {
+          config: { $arrayElemAt: ["$config", 0] },
+          termsCount: 1
+        }
+      },
+      {
+        $addFields: {
+          expectedSubjectsCount: { $size: { $ifNull: ["$config.subjects", []] } }
+        }
+      },
+      {
+        $addFields: {
+          expectedSubjectsCount: {
+            $cond: [
+              { $gt: ["$expectedSubjectsCount", 0] },
+              "$expectedSubjectsCount",
+              {
+                $switch: {
+                  branches: [
+                    { case: { $in: ["$_id.standard", ["11", "12"]] }, then: 6 },
+                    { case: { $in: ["$_id.standard", ["6", "7", "8", "9", "10"]] }, then: 5 }
+                  ],
+                  default: 5
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        $addFields: {
+          maximumMarks: {
+            $multiply: ["$termsCount", "$expectedSubjectsCount", 100]
+          }
+        }
+      },
+      {
+        $match: { maximumMarks: { $gt: 0 } }
+      },
+      {
+        $addFields: {
+          percentage: {
+            $round: [
+              { $multiply: [ { $divide: ["$totalScore", "$maximumMarks"] }, 100 ] },
+              2
+            ]
+          }
+        }
+      },
+      { $sort: { percentage: -1, totalScore: -1, "_id.name": 1 } },
       {
         $group: {
           _id: {
@@ -622,8 +691,11 @@ const getDashboardStats = async (req, res) => {
             section: "$_id.section",
             termName: "$_id.termName"
           },
+          studentId: { $first: "$_id.studentId" },
           topStudent: { $first: "$_id.name" },
-          topScore: { $first: "$totalScore" }
+          topScore: { $first: "$totalScore" },
+          maximumMarks: { $first: "$maximumMarks" },
+          percentage: { $first: "$percentage" }
         }
       },
       { $sort: { "_id.standard": 1, "_id.section": 1, "_id.termName": 1 } }
